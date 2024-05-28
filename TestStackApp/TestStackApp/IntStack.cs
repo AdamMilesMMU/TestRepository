dcaa33399404526f68452bbc922f88850100de6e
Adam-L-Miles <166828184+Adam-L-Miles@users.noreply.github.com>
2024-05-28 13:42:36 +0100

    Initial commit of basic stack functionality
    This is where I can write my nice changes for the stack program I have written, isn't that ruddy swell (y)

---
+++ TestStackApp/TestStackApp/IntStack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestStackApp
+{
+    internal class IntStack
+    {
+        int maxSize = 10;
+        int top = 0;
+        int[] store;
+
+        public IntStack()
+        {
+            store = new int[maxSize];
+        }
+
+        public IntStack(int maxSize)
+        {
+            this.maxSize = maxSize;
+        }
+
+
+        public void Push(int value)
+        {
+            store[top++] = value;
+        }
+
+        public int Pop()
+        {
+            return store[--top];
+
+        }
+
+        public bool IsEmpty()
+        {
+            return top == 0;
+        }
+
+        public bool IsFull()
+        {
+            return top == maxSize;
+        }
+
+
+    }
+}
---
+++ TestStackApp/TestStackApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestStackApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IntStack testStack = new IntStack();
+
+            int i = 1;
+
+            while (!testStack.IsFull())
+            {
+                testStack.Push(i++);
+            }
+
+            while (!testStack.IsEmpty())
+            {
+                Console.WriteLine(testStack.Pop());
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
